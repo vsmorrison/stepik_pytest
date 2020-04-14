568ec4c366d66e1d0dad8deb04b4693bc86e52a8
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2020-04-14 19:59:36 +0300

    minor syntax fixex

--- test_items.py
+++ test_items.py
@@ -1,9 +1,9 @@
 import time
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_find_add_item_to_cart_button(browser):
-    browser.get(link)
-    add_to_cart_button = browser.find_element_by_css_selector('.btn-add-to-basket')
-    assert add_to_cart_button.is_displayed(), "Button is unavailable"
+def test_find_add_button(browser):
+    browser.get(LINK)
+    add_button = browser.find_element_by_css_selector('.btn-add-to-basket')
+    assert add_button.is_displayed(), "Button is unavailable"
     time.sleep(5)
